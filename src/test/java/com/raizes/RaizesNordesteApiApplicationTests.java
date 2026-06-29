442782bfce1bf37e3560ab45a10efec64399c52a
Dimas Rocha <dimas.dasp@gmail.com>
2026-06-28 21:25:19 -0300

    project init

---
+++ src/test/java/com/raizes/RaizesNordesteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raizes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesNordesteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
